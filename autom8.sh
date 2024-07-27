60fced9cb8249e59034c2e6de4fcfa07dbc373b6
Marcio Moreira Junior <marcio.moreira@teladapters.com>
2024-07-27 18:22:02 -0300

    Adding stuff to desktop installation

--- autom8.sh
+++ autom8.sh
@@ -70,27 +70,35 @@ install_desktop() {
         sudo apt update && sudo apt upgrade -y
         echo "Atualização do sistema operacional finalizada!"
     fi
-
+    sleep 1
     clear
     echo "Instalando pacotes básicos"
     sudo apt install -y ntpdate zfsutils-linux tcpdump vim nmap net-tools iptables iptables-persistent git curl wget links ruby python3 python3-pip build-essential openssl traceroute pkg-config gcc make iptraf netcat-traditional zsh iperf htop iotop
     echo "Pacotes instalados."
 
+    sleep 1
     clear
     echo "Instalando ferramentas gráficas"
     sudo apt install -y gnome-shell-extension-manager gnome-software gnome-software-plugin-flatpak flatpak ubuntu-restricted-extras gnome-shell-extension-ubuntu-tiling-assistant gnome-extensions gnome-weather gnome-clocks gnome-tweaks fonts-firacode fonts-roboto fonts-cascadia-code chrome-gnome-shell
     echo "Pacotes instalados"
 
+    sleep 1
     clear
     echo "Instalando utilitários e ferramentas de desktop"
     sudo apt install -y rar unrar p7zip-full p7zip-rar tlp 
     echo "Pacotes Instalados"
 
+    sleep 1
     clear
     echo "Instalando ferramentas de trabalho"
     sudo apt install -y virtualbox ansible wireshark tshark gcc make tmux multitail timeshift openssh-server""
     echo "Pacotes Instalados"
-    
+
+    sleep 1
+    clear
+    echo "Atualizando editor de texto"
+    update-alternatives --config-editor
+
 }
 
 install_server() {
