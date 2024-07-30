fb6f27939d155412745e8c9f69fd6b845cb9217d
Marcio Moreira Jr. <mdmjunior@gmail.com>
2024-07-30 17:44:28 -0300

    added more repo

--- autom8.sh
+++ autom8.sh
@@ -3,17 +3,17 @@
 ########################################################
 # AutoM8 - Ubuntu Post-Install Automation Tool         #
 # Author: Marcio Moreira junior                        #
+# email: mdmjunior@gmail.com                           #
+# Versão 1.0                                           #
 ########################################################
-
-#!/bin/bash
 
 check_env() {
     # Verificar a distribuição e release
     DISTRO=$(lsb_release -is 2>/dev/null)
     RELEASE=$(lsb_release -rs 2>/dev/null)
     USERNM=$(whoami)
-
-    echo "[ ----------------------------------------------------- ]"
+   u
+    echo "  [ -------------------------------------------------- ]"
     echo " "
     echo "   █████╗ ██╗   ██╗████████╗ ██████╗ ███╗   ███╗ █████╗ "
     echo "  ██╔══██╗██║   ██║╚══██╔══╝██╔═══██╗████╗ ████║██╔══██╗"
@@ -21,10 +21,11 @@ check_env() {
     echo "  ██╔══██║██║   ██║   ██║   ██║   ██║██║╚██╔╝██║██╔══██╗"
     echo "  ██║  ██║╚██████╔╝   ██║   ╚██████╔╝██║ ╚═╝ ██║╚█████╔╝"
     echo "  ╚═╝  ╚═╝ ╚═════╝    ╚═╝    ╚═════╝ ╚═╝     ╚═╝ ╚════╝ "
-    echo "[ ----------------------------------------------------- ]"
-    echo " Ubuntu Post-Installation Tool "
+    echo "  [ -------------------------------------------------- ]"
+    echo "              Ubuntu Post-Installation Tool             "
+    echo "       https://marciomoreirajunior.com.br/AutoM8/       "
 
-
+    # Verifica se a distribuição é compatível
     if [ "$DISTRO" != "Ubuntu" ]; then
         echo "Distribuição não suportada: $DISTRO"
         exit 1
@@ -43,7 +44,7 @@ check_env() {
 
     if [ "$INSTALL_TYPE" == "desktop" ]; then
         install_desktop
-    elif [ "$INSTALL_TYPE" == "server" ]; then
+        elif [ "$INSTALL_TYPE" == "server" ]; then
         install_server
     else
         echo "Tipo de instalação inválido: $INSTALL_TYPE"
@@ -52,13 +53,12 @@ check_env() {
 }
 
 install_desktop() {
-    echo "Iniciando instalação otimizada para Desktop"
+    echo "INICIANDO INSTALAÇÃO OTIMIZADA PARA DESKTOP"
     echo "O script irá preparar o seu sistema operacional, aguarde..."
-    echo "Distro: $DISTRO"
-    echo "Release: $RELEASE"
-    echo "Usuario: $USERNM"
+    echo "DISTRO: $DISTRO"
+    echo "RELEASE: $RELEASE"
+    echo "USUARIO: $USERNM"
     sleep 1
-    clear
 
     # Atualiza os repositórios e o sistema operacional
 
@@ -66,77 +66,89 @@ install_desktop() {
         echo "Esse script deve ser executado com o seu usuario, $USERNM"
         exit 1
     else
-        echo "Atualizando os repositórios e pacotes do SO"
+        echo "ATUALIZANDO OS REPOSITÓRIOS E PACOTES DO SO"
         echo "Seu usuário não é root, digite a senha para elevação"
         sudo apt update && sudo apt upgrade -y
         echo "Atualização do sistema operacional finalizada!"
     fi
 
     sleep 1
-    echo "Instalando pacotes básicos"
+    echo "INSTALANDO PACOTES BÁSICOS"
     sudo apt install -y ntpdate vim net-tools curl wget links htop iotop openssh-server openssl tmux multitail zsh sshpass expect gpg dconf
     echo "Pacotes instalados."
 
     sleep 1
-    echo "Adicionando repositórios extras"
+    echo "ADICIONANDO REPOSITÓRIOS EXTRAS"
+    echo "INSTALANDO REPOSITORIO HASHICORP"
     wget -O- https://apt.releases.hashicorp.com/gpg | sudo gpg --dearmor -o /usr/share/keyrings/hashicorp-archive-keyring.gpg
     echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
+    echo "INSTALANDO REPOSITORIO TOR"
+    wget -qO- https://deb.torproject.org/torproject.org/A3C4F0F979CAA22CDBA8F512EE8CBC9E886DDD89.asc | sudo gpg --dearmor -o /usr/share/keyrings/deb.torproject.org-keyring.gpg
+    echo "deb [signed-by=/usr/share/keyrings/deb.torproject.org-keyring.gpg] https://deb.torproject.org/torproject.org $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/tor.list
     sudo apt update
     echo "Repositórios Instalados"
 
     sleep 1
-    echo "Instalando gerenciadores de pacotes"
+    echo "INSTALANDO GERENCIADORES DE PACOTES"
     sudo apt install -y nala gnome-software-plugin-flatpak flatpak gdebi snapd
+    sudo flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+    echo "OTIMIZANDO NALA"
+    sudo nala fetch
     echo "Pacotes instalados"
 
     sleep 1
-    echo "Instalando ferramentas de compactação de arquivos"
-    sudo apt install -y rar unrar p7zip-full p7zip-rar tlp bzip2 tar unzip
+    echo "INSTALANDO FERRAMENTAS DE COMPACTAÇÃO DE ARQUIVOS"
+    sudo apt install -y rar unrar p7zip-full p7zip-rar tlp tlp-rdw bzip2 tar unzip
+    sudo systemctl enable tlp
     echo "Pacotes instalados"
 
     sleep 1
-    echo "Instalando ferramentas para manipulaçao de filesystems"
-    sudo apt install -y zfsutils-linux samba-common-bin ntfs-3g
+    echo "INSTALANDO FERRAMENTAS PARA MANIPULAÇAO DE FILESYSTEMS"
+    sudo apt install -y zfsutils-linux samba-common-bin ntfs-3g libfuse2
     echo "Pacotes Instalados"
 
     sleep 1
-    echo "Instalando ferramentas de rede"
+    echo "INSTALANDO FERRAMENTAS DE REDE"
     sudo apt install -y tcpdump nmap zenmap iptables iptables-persistent traceroute iptraf netcat-traditional wireshark tshark iperf
     echo "Pacotes Instalados"
 
     sleep 1
-    echo "Instalando ferramenta de backup"
+    echo "INSTALANDO PACOTES DE SEGURANÇA E VPN"
+    sudo apt install -y network-manager-openvpn network-manager-openvpn-gnome openvpn
+
+    sleep 1
+    echo "INSTALANDO FERRAMENTA DE BACKUP"
     sudo apt install -y timeshift
     echo "Pacote Instalado"
 
     sleep 1
-    echo "Instalando addons do Gnome e fontes"
+    echo "INSTALANDO ADDONS DO GNOME E FONTES"
     sudo apt install -y gnome-shell-extension-manager gnome-software ubuntu-restricted-extras gnome-shell-extension-ubuntu-tiling-assistant gnome-weather gnome-clocks gnome-tweaks fonts-firacode fonts-roboto fonts-cascadia-code chrome-gnome-shell
     echo "Pacotes instalados"
 
     sleep 1
-    echo "Instalando ferramentas de virtualização"
+    echo "INSTALANDO FERRAMENTAS DE VIRTUALIZAÇÃO"
     sudo apt install -y virtualbox virtualbox-ext-pack virtualbox-dkms virtualbox-guest-utils virtualbox-guest-additions-iso
     echo "Pacotes instalados"
 
     sleep 1
-    echo "Instalando ferramentas de desenvolvimento"
+    echo "INSTALANDO FERRAMENTAS DE DESENVOLVIMENTO"
     sudo apt install -y apt-transport-https ca-certificates software-properties-common gcc make git ruby python3 python3-pip build-essential openssl pkg-config linux-headers-"$(uname -r)" linux-headers-generic libssl-dev
     echo "Pacotes instalados"
 
     sleep 1
-    echo "Instalando ferramentas de automação"
+    echo "INSTALANDO FERRAMENTAS DE AUTOMAÇÃO"
     sudo apt install -y ansible ansible-lint terraform packer vagrant
     echo "Pacotes instalados"
 
     sleep 1
-    echo "Instalando Google Chrome"
+    echo "INSTALANDO GOOGLE CHROME"
     wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
     sudo dpkg -i google-chrome-stable_current_amd64.deb
     echo "Pacote instalado"
 
     # Instalação de pacotes via Snap
-    echo "Instalação de softwares via Snap"
+    echo "INSTALAÇÃO DE SOFTWARES VIA SNAP"
     sudo snap install code --classic
     sudo snap install drawio
     sudo snap install firefox
@@ -157,25 +169,34 @@ install_desktop() {
     echo "Pacotes instalados"
 
     sleep 1
-    echo "Atualizando editor de texto"
-    sudo update-alternatives --set editor /usr/bin/vim
-    echo "Criando Link para o Python"
-    sudo ln -s /usr/bin/python3 /usr/bin/python
-    echo "Adicionando usuario ao grupo do vbox"
-    sudo usermod -aG vboxusers mmoreira
-
-    sleep 1
-    echo "instalando Stacer"
+    echo "INSTALANDO STACER"
     sudo nala install -y stacer
     echo "Stacer instalado"
 
-    echo "Sistema pronto para uso"
+    sleep 1
+    echo "ATUALIZANDO EDITOR DE TEXTO"
+    sudo update-alternatives --set editor /usr/bin/vim
+    echo "CRIANDO LINK PARA O PYTHON"
+    sudo ln -s /usr/bin/python3 /usr/bin/python
+    echo "ADICIONANDO USUARIO AO GRUPO DO VBOX"
+    sudo usermod -aG vboxusers mmoreira
+    echo "HABILITANDO MINIMIZAR NO CLICK DO MOUSE"
+    gsettings set org.gnome.shell.extensions.dash-to-dock click-action 'minimize'
+
+    # Removendo pacotes não utilizados
+    echo "REMOVENDO PACOTES DESNECESSÁRIOS"
+    sudo apt remove -y --purge apport apport-gtk rhythmbox
+    sudo apt autoremove -y
+    sudo apt autoclean -y
+
+
+    echo "SISTEMA PRONTO PARA USO"
     exit
 }
 
 install_server() {
-    echo "Função de instalação do servidor"
-    # Adicione aqui os comandos para instalar o servidor
+    echo "Função de instalação do servidor ainda não implementada"
+    exit 1
 }
 
 # Chamar a função check_env
