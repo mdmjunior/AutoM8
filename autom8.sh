8a9bde5f95667dba3150961e7c03cd0e60e5123b
Marcio Moreira Jr. <mdmjunior@gmail.com>
2024-08-01 21:56:14 -0300

    typo

--- autom8.sh
+++ autom8.sh
@@ -189,7 +189,7 @@ install_desktop() {
     sudo snap install firmware-updater
     echo -e "\e[32mPacotes Instalados\e[0m"
 
-    echo "INSTALANDO STACER"con
+    echo "INSTALANDO STACER"
     sudo nala install -y stacer
     echo -e "\e[32mPacotes Instalados\e[0m"
 
