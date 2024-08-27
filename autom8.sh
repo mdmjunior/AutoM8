98178dff9e7c1b06a01603f77407b4d0d9c94c7e
Marcio Moreira Jr. <mdmjunior@gmail.com>
2024-08-27 19:04:56 -0300

    Changed code

--- autom8.sh
+++ autom8.sh
@@ -56,11 +56,11 @@ install_desktop() {
     fi
 
     echo "INSTALANDO PACOTES BÁSICOS"
-    sudo apt install -y ntpdate vim net-tools iproute2 curl sshpass ethtool colordiff wget links htop iotop openssh-server openssl tmux multitail zsh sshpass expect gpg dconf-cli dconf-editor smartmontools linux-tools-generic
+    sudo apt install -y ntpdate vim net-tools iproute2 curl sshpass ethtool wget links htop iotop openssh-server openssl tmux multitail zsh sshpass expect gpg dconf-cli dconf-editor linux-tools-generic
     echo -e "\e[32mPacotes instalados\e[0m"
 
     echo "INSTALANDO GERENCIADORES DE PACOTES"
-    sudo apt install -y nala gnome-software-plugin-flatpak flatpak gdebi snapd
+    sudo apt install -y gnome-software-plugin-flatpak flatpak synaptic snapd
     echo -e "\e[32mGerenciadores de pacotes instalados\e[0m"
 
     echo "ADICIONANDO REPOSITÓRIOS EXTRAS"
@@ -76,17 +76,13 @@ install_desktop() {
     echo "INSTALANDO REPOSITÓRIO FLATPAK"
     sudo flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 
-    echo "OTIMIZANDO REPOSITÓRIOS NALA"
-    sudo nala fetch
-    echo -e "\e[32mRepositórios Instalados e Atualizados\e[0m"
-
     echo "INSTALANDO GOOGLE CHROME"
     wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
     sudo dpkg -i google-chrome-stable_current_amd64.deb
     echo -e "\e[32mPacotes Instalados\e[0m"
 
     echo "INSTALANDO FERRAMENTAS DE COMPACTAÇÃO DE ARQUIVOS"
-    sudo apt install -y rar unrar p7zip-full p7zip-rar bzip2 tar unzip
+    sudo apt install -y rar unrar bzip2 tar unzip
     echo -e "\e[32mPacotes instalados\e[0m"
 
     echo "INSTALANDO PACOTES DE FILESYSTEMS"
@@ -106,20 +102,17 @@ install_desktop() {
     echo -e "\e[32mPacotes Instalados\e[0m"
 
     echo "INSTALANDO ADDONS DO GNOME E FONTES"
-    sudo apt install -y gnome-shell-extension-manager gnome-software ubuntu-restricted-extras gnome-shell-extension-ubuntu-tiling-assistant gnome-backgrounds gnome-weather gnome-clocks gnome-tweaks fonts-firacode fonts-roboto fonts-cascadia-code chrome-gnome-shell
+    sudo apt install -y gnome-shell-extension-manager gnome-software ubuntu-restricted-extras gnome-shell-extension-ubuntu-tiling-assistant gnome-backgrounds gnome-clocks gnome-tweaks fonts-firacode fonts-roboto fonts-cascadia-code chrome-gnome-shell
     echo -e "\e[32mPacotes Instalados\e[0m"
 
     echo "INSTALANDO EXTENSÕES DO GNOME"
     extensions=( https://extensions.gnome.org/extension/4269/alphabetical-app-grid/
-            https://extensions.gnome.org/extension/6682/astra-monitor/
-            https://extensions.gnome.org/extension/3193/blur-my-shell/
             https://extensions.gnome.org/extension/517/caffeine/
             https://extensions.gnome.org/extension/779/clipboard-indicator/
             https://extensions.gnome.org/extension/2087/desktop-icons-ng-ding/
             https://extensions.gnome.org/extension/1301/ubuntu-appindicators/
             https://extensions.gnome.org/extension/1300/ubuntu-dock/
-            https://extensions.gnome.org/extension/19/user-themes/
-            https://extensions.gnome.org/extension/21/workspace-indicator/ )
+            https://extensions.gnome.org/extension/19/user-themes/ )
 
     for i in "${extensions[@]}"
     do
@@ -136,16 +129,11 @@ install_desktop() {
     echo -e "\e[32mPacotes Instalados\e[0m"
 
     echo "INSTALANDO FERRAMENTAS DE DESENVOLVIMENTO"
-    sudo apt install -y apt-transport-https ca-certificates software-properties-common gcc make git ruby python3 python3-pip python3.12-venv build-essential libglib2.0-dev-bin openssl pkg-config linux-headers-"$(uname -r)" linux-headers-generic libssl-dev
+    sudo apt install -y apt-transport-https ca-certificates software-properties-common gcc make git ruby python3 python3-pip python3.12-venv build-essential libglib2.0-dev-bin pkg-config linux-headers-"$(uname -r)" linux-headers-generic libssl-dev
     echo -e "\e[32mPacotes Instalados\e[0m"
 
     echo "INSTALANDO FERRAMENTAS DE VIRTUALIZAÇÃO"
     sudo apt install -y virtualbox virtualbox-ext-pack virtualbox-dkms virtualbox-guest-utils virtualbox-guest-additions-iso virtualbox-guest-x11
-    echo -e "\e[32mPacotes Instalados\e[0m"
-
-    echo "INSTALANDO DOCKER"
-    sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
-    sudo systemctl enable docker
     echo -e "\e[32mPacotes Instalados\e[0m"
 
     echo "INSTALANDO FERRAMENTAS DE AUTOMAÇÃO"
@@ -159,20 +147,14 @@ install_desktop() {
     sudo snap install go --classic
     sudo snap install joplin-desktop
     sudo snap install lxd
-    sudo snap install neofetch-desktop
     sudo snap install smart-file-renamer
     sudo snap install spotify
     sudo snap install sublime-text --classic
     sudo snap install wonderwall
     sudo snap install gnome-system-monitor
-    sudo snap install cheese
     sudo snap install gtk-theme-orchis
     sudo snap install gtk-common-themes
     sudo snap install firmware-updater
-    echo -e "\e[32mPacotes Instalados\e[0m"
-
-    echo "INSTALANDO STACER"
-    sudo nala install -y stacer
     echo -e "\e[32mPacotes Instalados\e[0m"
 
     echo "ATUALIZANDO EDITOR DE TEXTO PADRÃO"
@@ -195,11 +177,6 @@ install_desktop() {
     read -r GIT_EMAIL
     git config --global user.name "$GIT_REALNAME"
     git config --global user.email "$GIT_EMAIL"
-
-    echo "RESTAURANDO CONFIGURAÇÕES DO GNOME"
-    git clone https://github.com/mdmjunior/AutoM8.git
-    cd AutoM8 || exit
-    dconf load -f / < saved_settings.dconf
 
     # Removendo pacotes não utilizados
     echo "REMOVENDO PACOTES DESNECESSÁRIOS"
